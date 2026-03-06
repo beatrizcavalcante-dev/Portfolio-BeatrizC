d3f0f0ae5704a434707e8ec8493aa3951a9d4b91
Biagibi <162746216+Biagibi@users.noreply.github.com>
2026-03-05 21:18:15 -0300

    finally awww

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault()
+
+    const section = document.querySelector(this.getAttribute('href'))
+
+    section.scrollIntoView({
+      behavior: "smooth"
+    })
+  })
+})
+
+const btnTopo = document.getElementById("btn-topo")
+
+window.addEventListener("scroll", function(){
+
+  if(window.scrollY > 300){
+    btnTopo.style.display = "block"
+  } else{
+    btnTopo.style.display = "none"
+  }
+
+})
+
+btnTopo.addEventListener("click", function(){
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+
+})
+
+const botaoDark = document.getElementById("dark-mode-toggle")
+
+botaoDark.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode")
+})
+
 function virarCard(card) {
   card.classList.toggle("virado")
 }
